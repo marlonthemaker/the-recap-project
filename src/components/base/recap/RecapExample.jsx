3dbe68e28e89913f5f9eafecd1749b45013d58c3
Marlon Frausto <marlonthemaker@gmail.com>
2025-02-02 20:44:45 +0000

    added simple components for abridged version of recap

--- src/components/base/recap/RecapExample.jsx
+++ src/components/base/recap/RecapExample.jsx
@@ -1,3 +1,4 @@
+import TeamHeader from './TeamHeader';
 const posts = [
   {
     id: 1,
@@ -41,25 +42,27 @@ export default function RecapExample() {
                   </a>
                 </div>
                 <div className="group relative">
-                  <h3 className="mt-3 text-lg/6 font-semibold text-gray-900 group-hover:text-gray-600">
-                    <a href={post.href}>
-                      <span className="absolute inset-0" />
-                      {post.title}
-                    </a>
-                  </h3>
+                  <TeamHeader/>
                   <p className="mt-5 line-clamp-3 text-sm/6 text-gray-600">{post.description}</p>
                 </div>
-                <div className="relative mt-8 flex items-center gap-x-4">
-                  <img alt="" src={post.author.imageUrl} className="size-10 rounded-full bg-gray-50" />
-                  <div className="text-sm/6">
-                    <p className="font-semibold text-gray-900">
-                      <a href={post.author.href}>
-                        <span className="absolute inset-0" />
-                        {post.author.name}
-                      </a>
-                    </p>
-                    <p className="text-gray-600">{post.author.role}</p>
-                  </div>
+{
+                // <div className="relative mt-8 flex items-center gap-x-4">
+                //   <img alt="" src={post.author.imageUrl} className="size-10 rounded-full bg-gray-50" />
+                //   <div className="text-sm/6">
+                //     <p className="font-semibold text-gray-900">
+                //       <a href={post.author.href}>
+                //         <span className="absolute inset-0" />
+                //         {post.author.name}
+                //       </a>
+                //     </p>
+                //     <p className="text-gray-600">{post.author.role}</p>
+                //   </div>
+                // </div>
+}
+                <div className="text-sm pt-4">
+                  <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
+                    View all<span className="sr-only"> {"view all"} stats</span>
+                  </a>
                 </div>
               </article>
             ))}
---
+++ src/components/base/recap/TeamHeader.jsx
@@ -0,0 +1,18 @@
+export default function Example() {
+  return (
+    <div className="flex">
+      <div className="shrink-0 self-center">
+        <img alt="" src={"https://midfield.mlbstatic.com/v1/team/114/spots/144"} className="size-10 rounded-full bg-gray-50" />
+      </div>
+      <div className="text-center">
+        <p className="mt-1">
+          4 - 0
+        </p>
+        <h4 className="text-lg font-bold">final</h4>
+      </div>
+      <div className="shrink-0 self-center">
+        <img alt="" src={"https://midfield.mlbstatic.com/v1/team/117/spots/144"} className="size-10 rounded-full bg-gray-50" />
+      </div>
+    </div>
+  )
+}
